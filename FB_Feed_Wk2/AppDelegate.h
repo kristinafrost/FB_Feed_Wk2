1aa55ee6e01b2a86a0db6e28d6f51bbc85229d6e
Kristina Frost <kristinafrost@tw-mbp15retina-kfrost.local>
2014-06-14 11:56:19 -0700

    Initial Commit

---
+++ FB_Feed_Wk2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FB_Feed_Wk2
+//
+//  Created by Kristina Frost on 6/14/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FB_Feed_Wk2Tests/FB_Feed_Wk2Tests.m
@@ -0,0 +1,34 @@
+//
+//  FB_Feed_Wk2Tests.m
+//  FB_Feed_Wk2Tests
+//
+//  Created by Kristina Frost on 6/14/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FB_Feed_Wk2Tests : XCTestCase
+
+@end
+
+@implementation FB_Feed_Wk2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
